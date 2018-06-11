f6a6d1cdf7b8920eac73cd07cb5c12dc40d19bd8
unknown <bubbastinx@gmail.com>
2018-06-10 19:05:33 -0600

    building out initial file structure for project

---
+++ server.js
@@ -0,0 +1,10 @@
+
+//Module dependencies 
+
+let express  = require('express')
+let app  = require()
+let passport = require('passport')
+let session = require('express-session')
+let bodyParser = require('body-parser')
+let env = require('dotenv')
+let exphbs = require('express-handlebars')
